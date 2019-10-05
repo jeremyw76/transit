78e43850f83daf8b0a157ae6697015dbbb3be59f
Jeremy Wiebe <jeremyw76@live.com>
2019-10-05 14:46:10 -0500

    Seeded db
    - Fixed seeds script and seeded stops and streets.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,6 @@ stops = JSON.parse(response.body)['stops']
 BusStop.destroy_all
 Street.destroy_all
 
-stops.each do |stop|
-  street = get_or_add_street(stop['street'])
-  cross_street = get_or_add_street(stop['cross-street'])
-  position = stop['geographic']
-
-  BusStop.create(
-    number: stop['key'],
-    name: stop['name'],
-    direction: stop['direction'],
-    side: stop['side'],
-    street: street,
-    cross_street: cross_street,
-    latitude: position['latitude'],
-    longitude: positoin['longitude']
-  )
-end
-
 def get_or_add_street(street)
   street_key = street['key']
   street_object = Street.find_by_key(street_key)
@@ -35,4 +18,21 @@ def get_or_add_street(street)
   end
 
   return street_object
+end
+
+stops.each do |stop|
+  street = get_or_add_street(stop['street'])
+  cross_street = get_or_add_street(stop['cross-street'])
+  position = stop['centre']['geographic']
+
+  BusStop.create(
+    number: stop['key'],
+    name: stop['name'],
+    direction: stop['direction'],
+    side: stop['side'],
+    street: street,
+    cross_street: cross_street,
+    latitude: position['latitude'],
+    longitude: position['longitude']
+  )
 end
