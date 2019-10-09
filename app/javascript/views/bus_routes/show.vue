707dad4f270db8cc1d0e260d1c5ab40356295d80
jeremyw76 <jeremyw76@live.com>
2019-10-09 13:13:51 -0500

    Began adding vue to the project
    - Added vue to the project.
    - Began experimentation with a bus_routes component for filtering stops
    by variant.
    - The bus_routes show view will need to have a mounting point for this
    component added to it.
    - This component is not yet properly constructed.

---
+++ app/javascript/views/bus_routes/show.vue
@@ -0,0 +1,42 @@
+<script>
+  import CSRF from 'components/shared/csrf.vue';
+
+  export default {
+    components: {
+      csrf: CSRF
+    },
+    props: {
+      variants: Array
+    },
+    data: function() {
+      return ({
+        variant: {
+          key: '',
+          name: '',
+        },
+        rules: {
+
+        }
+      })
+    }
+  }
+  </script>
+
+  <template>
+    <div>
+      <div class="field is-grouped">
+        variants.foreach() => {
+          <p class="control">
+            <a class="button is-link">
+              variant.name
+            </a>
+          </p>
+        }
+      </div>
+      <h2>Stops</h2>
+
+    </div>
+  </template>
+
+  <style scoped>
+  </style>
