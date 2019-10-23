1efbcd28cf994b7fa99c03a8e8bd45e64774bdcd
Jeremy Wiebe <jeremyw76@live.com>
2019-10-23 15:40:42 -0500

    Fixed streets index view
    - Added search bar to streets index view -- functionality is not
    complete but it works. Would be nice to implement a fuzzy search.
    - Added streets to main navigation.
    - Fixed styling on streets index view.

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -7,4 +7,17 @@ class StreetsController < ApplicationController
     @street = Street.find(params[:id])
     @cross_street_stops = BusStop.where(cross_street: @street)
   end
+
+  def search
+    name = params[:name]
+    street = Street.where(name: name).first
+
+    if (street == nil) then
+      
+    end
+
+    if (street != nil) then
+      redirect_to street_path(street.id)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'streets', to: "streets#index"
   get 'streets/:id' => 'streets#show', as: 'street', id: /\d+/
 
+  post 'streets/find' => 'streets#search', as: 'search_streets'
+
   get 'stops', to: "bus_stops#index"
   get 'stops/:id' => 'bus_stops#show', as: 'stop', id: /\d+/
 
